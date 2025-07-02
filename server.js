bcf453f9f76b39960dc5a39b4118d21cf6c0efb2
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-07-02 18:28:28 +0530

    create the basic server to running.

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).json({success:false,message:"Internal Server Error"});
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
 
